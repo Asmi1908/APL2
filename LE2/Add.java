de53be9e682231fb5361e4b22a0769717ea11d33
241B053 <241B053@juet.co.in>
2026-08-03 15:46:56 +0530

    This is the solution to 2nd Lab

---
+++ LE2/Add.java
@@ -0,0 +1,12 @@
+class Add{
+    public int table(int m){
+        int i = 1;
+        while(i<=10){
+            int p = 2*i;
+            System.out.println(m+" X "+i+" = "+p);
+            i = i+1;
+            
+        }
+        return 0;
+    }
+}
